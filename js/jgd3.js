d401638f896509076535caf33cd1178883eae8ea
Josh Gilfillan <j.gilfillan+dev@gmail.com>
2014-08-22 21:51:57 +1000

    initial set of files in repo

---
+++ js/jgd3.js
@@ -0,0 +1,143 @@
+var jgd3 = new JGD3;
+
+function JGD3() {
+  // properties
+  this.datasets = [];
+}
+
+// returns the index of a given obj in a given array.  optionally accepts an accessor function.
+JGD3.prototype.indexOf = function (obj, array, accessor) {
+  if (accessor === undefined) {
+    accessor = function(d) {return d;};
+  }
+
+  return array.map(accessor).indexOf(obj);
+}
+
+function Dataset(data, fileName) {
+  this.data = data;
+  this.sourceFileName = fileName;
+  this.emptyV = this.getEmptyVariables(this.data);
+  this.continuousV = this.getContinuousVariables(this.data, this.emptyV);
+  this.timeseriesV = [];
+  this.discreteV = this.getDiscreteVariables(this.data, this.emptyV);
+  this.xV = '';
+  this.yV = '';
+  this.colourV = '';
+  this.sizeV = '';
+  this.filterV = [];
+}
+
+Dataset.prototype.getEmptyVariables = function(data) {
+  var keys = Object.keys(data[0]),
+      emptyVariables = [];
+      ;
+
+  keys.map(function(key) {
+    var isEmpty = data.reduce(function(a, b) {
+      if (b[key].trim() !== '') { return false; } else { return a; }
+    }, true);
+
+    if (isEmpty) { emptyVariables.push(key); }
+  });
+
+  return emptyVariables;
+}
+
+Dataset.prototype.getContinuousVariables = function(data, emptyList) {
+  var keys = Object.keys(data[0]).filter(function(d) { return (emptyList.indexOf(d) === -1); }),
+      continuousVariables = [];
+      ;
+
+  keys.map(function(key) {
+    var isContinuous = data.reduce(function(a, b) {
+      if (isNaN(b[key])) { return false; } else { return a; }
+    }, true);
+
+    if (isContinuous) { continuousVariables.push(key); }
+  });
+
+  return continuousVariables;
+}
+
+Dataset.prototype.getDiscreteVariables = function(data, emptyList) {
+  var keys = Object.keys(data[0]).filter(function(d) { return (emptyList.indexOf(d) === -1); }),
+      discreteVariables = [];
+      ;
+
+  keys.map(function(key) {
+    var isDiscrete = data.reduce(function(a, b) {
+      if (isNaN(b[key])) { return true; } else { return a; }
+    }, false);
+
+    if (isDiscrete) { discreteVariables.push(key); }
+  });
+
+  return discreteVariables;
+}
+
+
+
+function dropBoxOnClick() {
+  var dropBoxOptions = {
+  success: readDropBoxFile,
+  linkType: 'direct',
+  multiselect: false,
+  extensions: ['.csv']
+  }
+
+  Dropbox.choose(dropBoxOptions);
+}
+
+function readLocalFile() {
+  // console.log(document.getElementById('csvFile').files[0].name);
+  csvData = Papa.parse(document.getElementById('csvFile').files[0], {
+    delimiter: "",
+    header: true,
+    dynamicTyping: false,
+    preview: 0,
+    step: undefined,
+    encoding: "",
+    worker: false,
+    comments: false,
+    complete: processCSV(document.getElementById('csvFile').files[0].name),    //callback function
+    download: false
+    }
+  );
+
+}
+
+function readDropBoxFile(files) {
+  // console.log(files[0].name);
+  csvData = Papa.parse(files[0].link, {
+    delimiter: "",
+    header: true,
+    dynamicTyping: false,
+    preview: 0,
+    step: undefined,
+    encoding: "",
+    worker: false,
+    comments: false,
+    complete: processCSV(files[0].name),    //callback function
+    download: true
+    }
+  );
+}
+
+function processCSV(fileName) {
+  return function(results, file) {
+
+    console.log(fileName, results);
+
+    // check file with same name not loaded
+    if (jgd3.indexOf(fileName, jgd3.datasets, function(d) { return d.sourceFileName; }) === -1) {
+      jgd3.datasets.push(new Dataset(results.data, fileName));
+    }
+
+    // popups to alert user if conditions not met
+    else {
+      window.alert("File names must be unique!");
+    }
+  };
+}
+
